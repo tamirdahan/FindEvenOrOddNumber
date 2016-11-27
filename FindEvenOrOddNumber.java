3ffccf382512a876fdbbcce5afc2ca1e10d62532
tamirdahan <tamir.dahan@ness-tech.co.il>
2016-11-27 15:03:12 +0200

    Add files via upload

---
+++ FindEvenOrOddNumber.java
@@ -0,0 +1,27 @@
+public class FindEvenOrOddNumber
+ {
+ 
+        public static void main(String[] args)
+		{
+               
+                //create an array of 10 numbers
+                int[] numbers = new int[]{1,2,3,4,5,6,7,8,9,10};
+               
+                for(int i=0; i < numbers.length; i++)
+				{
+                       
+                        /*
+                         * use modulus operator to check if the number is even or odd. 
+                         * If we divide any number by 2 and reminder is 0 then the number is
+                         * even, otherwise it is odd.
+                         */
+                         
+                         if(numbers[i]%2 == 0)
+                                System.out.println(numbers[i] + " is even number.");
+                         else
+                                System.out.println(numbers[i] + " is odd number.");
+                               
+                }
+               
+        }
+}
